8d539a9c85e08a8de2d92a9949c9fe92542c0d01
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-04-11 09:49:02 -0700

    Add basic calculator functions Validate functions work in console

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+// Calculator buton functions - need to review for functionality
+// this is a starting point 
+
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+// const sum = function (arr) {
+//     return sumArray = arr.reduce((acc, current) =>
+//         acc + current, 0);
+// };
+
+// const multiply = function (arr) {
+//     return productArray = arr.reduce((acc, current) =>
+//         acc * current, 1);
+// };
+
+// const divide = function (arr) {
+//     return productArray = arr.reduce((acc, current) =>
+//         acc / current, 1);
+// }
