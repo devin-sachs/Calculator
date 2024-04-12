043919d57efc30900a527094b288e1f264ba5048
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-04-11 17:00:13 -0700

    Add skeleton for event listener logic with buttons

--- script.js
+++ script.js
@@ -20,6 +20,65 @@ const divide = function (a, b) {
     return a / b;
 }
 
+//variables to update display 
+
+let inputOne;
+let inputTwo;
+let operator;
+let displayValue;
+
+//
+
+const btn_container = document.getElementById('btn-container');
+btn_container.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    }
+
+    textBox = querySelector('textbox');
+
+    if (event.target.id.toLowerCase() === 'one') {
+
+    }
+
+    else if (event.target.id.toLowerCase() === 'two') {
+
+    }
+
+    else if (event.target.id.toLowerCase() === 'three') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'four') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'five') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'six') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'seven') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'eight') {
+
+    }
+    else if (event.target.id.toLowerCase() === 'nine') {
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
 // const sum = function (arr) {
 //     return sumArray = arr.reduce((acc, current) =>
 //         acc + current, 0);
