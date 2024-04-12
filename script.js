4bcd05348d8191fba3c4893481070fc92912d8c6
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-04-11 20:23:10 -0700

    Add JS logic for numeric buttons to display text output on display
    Add appropriate HTML ID's for JS selectors

--- script.js
+++ script.js
@@ -29,44 +29,69 @@ let displayValue;
 
 //
 
-const btn_container = document.getElementById('btn-container');
+textBox = document.getElementById('textbox');
+//document.body.appendChild(textBox); // this is not neccesary 
+
+let btn_container = document.getElementById('btn-container');
 btn_container.addEventListener('click', (event) => {
     const isButton = event.target.nodeName === 'BUTTON';
     if (!isButton) {
         return;
     }
 
-    textBox = querySelector('textbox');
-
-    if (event.target.id.toLowerCase() === 'one') {
-
+    if (event.target.id.toLowerCase() === 'zero') {
+        textBox.value = textBox.value + '0';
     }
-
+    else if (event.target.id.toLowerCase() === 'one') {
+        textBox.value = textBox.value + '1';
+    }
     else if (event.target.id.toLowerCase() === 'two') {
-
+        textBox.value = textBox.value + '2';
     }
-
     else if (event.target.id.toLowerCase() === 'three') {
-
+        textBox.value = textBox.value + '3';
     }
     else if (event.target.id.toLowerCase() === 'four') {
-
+        textBox.value = textBox.value + '4';
     }
     else if (event.target.id.toLowerCase() === 'five') {
-
+        textBox.value = textBox.value + '5';
     }
     else if (event.target.id.toLowerCase() === 'six') {
-
+        textBox.value = textBox.value + '6';
     }
     else if (event.target.id.toLowerCase() === 'seven') {
-
+        textBox.value = textBox.value + '7';
     }
     else if (event.target.id.toLowerCase() === 'eight') {
-
+        textBox.value = textBox.value + '8';
     }
     else if (event.target.id.toLowerCase() === 'nine') {
-
+        textBox.value = textBox.value + '9';
     }
+    else if (event.target.id.toLowerCase() === 'add') {
+        textBox.value = textBox.value + '+';
+    }
+    else if (event.target.id.toLowerCase() === 'subtract') {
+        textBox.value = textBox.value + '-';
+    }
+    else if (event.target.id.toLowerCase() === 'multiply') {
+        textBox.value = textBox.value + '*';
+    }
+    else if (event.target.id.toLowerCase() === 'divide') {
+        textBox.value = textBox.value + '/';
+    }
+    else if (event.target.id.toLowerCase() === 'decimal') {
+        textBox.value = textBox.value + '.';
+    }
+    else if (event.target.id.toLowerCase() === 'equals') {
+        textBox.value = textBox.value + '=';
+    }
+    else if (event.target.id.toLowerCase() === 'clear') {
+        textBox.value = '';
+    }
+
+
 
 })
 
